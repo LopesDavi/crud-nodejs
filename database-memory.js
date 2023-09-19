8a3a610694c983c04e3b4ee752c04fb097be6345
Davi Lopes <Davizau10@gmail.com>
2023-09-19 18:09:12 -0300

    3º commit - "create routes + test http methods"

--- database-memory.js
+++ database-memory.js
@@ -1,30 +1,37 @@
 //Arquivo responsável por criar DB em memória
-import { randomUUID } from 'node:crypto';
+import { randomUUID } from "node:crypto";
 
 export class DatabaseMemory {
   #videos = new Map();
 
   //Método responsável por listar todos os vídeos
   list() {
-    return this.#videos.values();
+    //Converter para um array de dados e mapear esse array e pegar o id e as demais informações
+    return Array.from(this.#videos.entries()).map((videoArray) => {
+      const id = videoArray[0];
+      const data = videoArray[1];
+
+      return {
+        id,
+        ...data,
+      };
+    });
   }
 
-
   //Método responsável por receber o vídeo e salvar dentro dos #videos
-  create(video){
+  create(video) {
     const videoId = randomUUID();
 
     this.#videos.set(videoId, video);
   }
 
   //Método responsável por receber o id do vídeo e salvar dentro dos #videos
-  update(id, video){
+  update(id, video) {
     this.#videos.set(id, video);
   }
 
   //Método responsável por deletar o vídeo através do id e salvar dentro dos #videos
-  delete(id){
+  delete(id) {
     this.#videos.delete(id);
   }
-
 }
--- server.js
+++ server.js
@@ -8,28 +8,56 @@ const database = new DatabaseMemory();
 
 //Ao utilizar métodos HTTP, podemos colocar rotas iguais para métodos diferentes
 
+//Ao utilizar POST e PUT, é possível enviar um 'corpo' para requisição (onde enviamos os dados de um form), isso é chamado de Request Body
+
 //Route POST - Create video
-server.post('/videos', () => {
-  return "Hello word!";
+server.post('/videos', (request, reply) => {
+
+  const {title, duration, description} = request.body;
+
+  database.create({
+    title: title,
+    description: description,
+    duration: duration,
+  });
+
+  return reply.status(201).send();
 })
 
 //Route GET - View video
-server.post('/videos', () => {
-  return "Hello word!";
+server.get('/videos', () => {
+  const videos =  database.list();
+
+  return videos;
 })
 
 //Route PUT - Update video
 /*Quando vamos atualizar um vídeo, precisamos passar um parâmetro na rota 
 (Route Paramater) para identificar esse vídeo (id)*/
-server.put('/videos/:id', () => {
-  return "Hello word!";
+server.put('/videos/:id', (request, reply) => {
+  //Para pegar esse id, tenho que requerer do route params o id
+  const videoId = request.params.id;
+
+  const {title, duration, description} = request.body;
+
+  database.update(videoId, {
+    title,
+    duration,
+    description,
+  });
+
+  return reply.status(204).send();
 })
 
 //Route DELETE - Delete video
 /*Quando vamos deletar um vídeo, precisamos passar um parâmetro na rota 
 (Route Paramater) para identificar esse vídeo (id)*/
-server.delete('/videos/:id', () => {
-  return "Hello word!";
+server.delete('/videos/:id', (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return reply.status(204).send();
 })
 
 server.listen({
